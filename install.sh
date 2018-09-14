f54196db7994ebee93a82042526dc45cadb05d20
xavfan <xavier.fanton@hotmail.fr>
2018-09-14 19:18:48 +0200

    ok

--- install.sh
+++ install.sh
@@ -1,4 +1,30 @@
-apt-get update
-apt-get install git
-apt-get 
-ca roule
+sudo apt-get update
+
+# on raspbian already installed packages:
+# sudo apt-get install build-essential bluez
+sudo apt-get install python-dbus git autoconf libtool libudev-dev \
+                     libncurses5-dev swig python-dev python-numpy
+
+cd ~
+mkdir src && cd src
+git clone https://github.com/dvdhrm/xwiimote.git
+git clone https://github.com/dvdhrm/xwiimote-bindings.git
+git clone https://github.com/chaosbiber/wiiweigh.git
+cd xwiimote
+./autogen.sh
+make
+sudo make install
+cd ../xwiimote-bindings
+./autogen.sh
+make
+sudo make install
+cd ../wiiweigh
+
+sudo gpasswd -a pi bluetooth # add user pi to bluetooth group
+# reboot or the wiiweight script will throw an exception
+# when trying to disconnect as normal user
+
+#sudo bluetoothctl
+
+# continue with bluetooth setup below
+cd ~
