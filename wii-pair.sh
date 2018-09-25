e4e800f9254f03d7fb311b3aa086bd9590170268
xavfan <33391012+xavfan@users.noreply.github.com>
2018-09-25 22:25:42 +0200

    Update wii-pair.sh

--- wii-pair.sh
+++ wii-pair.sh
@@ -9,3 +9,19 @@ Scan on
 pair $wii_address
 connect $wii_address
 trust $wii_address
+
+
+#!/bin/bash
+echo -e 'power on\n'
+sleep 2
+echo -e 'agent on\n'
+sleep 2
+echo -e 'scan on\n'
+sleep 2
+echo -e 'pair 00:1E:35:FC:C3:1F\n'
+sleep 4
+echo -e 'connect 00:1E:35:FC:C3:1F\n'
+sleep 5
+echo -e 'trust 00:1E:35:FC:C3:1F\n'
+sleep 5
+echo -e 'quit\n'
